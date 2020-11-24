ca6c04e1a3329917db04788d5787b44198566342
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-24 11:40:19 -0300

    configuration Cloudinary et Active storage

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,8 @@ class Shop < ApplicationRecord
   has_many :products
   has_many :order_shops
 
+  has_one_attached :photo
+
   validates :name, :address, :phone_number, :opening_hour, :closing_hour, :category, presence: true
   validates :opening_days, inclusion: { in: [1, 2, 3, 4, 5, 6, 7] }
 end
