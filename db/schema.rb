8f3fa75455de678c5ce942423d590ea9c2bd1280
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-26 14:20:19 -0300

    schema a été modifié

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_174428) do
+ActiveRecord::Schema.define(version: 2020_11_26_120245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2020_11_24_174428) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "total_price_cents", default: 0, null: false
+    t.string "checkout_session_id"
+    t.integer "amount_cents", default: 0, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
