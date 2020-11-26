b048ab0d81b291b9854f7be9bf38919abfb1255d
Nina Dissard <dissardnina@gmail.com>
2020-11-26 18:34:03 +0100

    Add a map to the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @shops = Shop.all
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @shops.geocoded.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude
+      }
+    end
   end
 end
