a9a39a46b9034cccbd8b68f61b33cc61dfbe3e9a
Caroline Huguet <caroline.huguet92@gmail.com>
2020-11-26 18:45:29 +0100

    implement js

---
+++ app/javascript/custome/script.js
@@ -0,0 +1,51 @@
+
+
+// const toggleBasket = () => {
+
+   // const controls = document.getElementById("card-control")
+   // controls.addEventListener('click', () => {
+    // alert("coucou");
+   // });
+
+//   const basket = document.getElementById("basket");
+//   const add = document.getElementById("add");
+//   const counter = document.getElementById("container-counter");
+
+
+//   if (basket.style.display === "none") {
+//     basket.style.display = "block";
+//     add.style.display = "none";
+//     counter.style.display = "none";
+//   } else {
+//     basket.style.display = "none";
+//     add.style.display = "block";
+//     counter.style.display = "flex";
+//   }
+// }
+
+
+// export { toggleBasket };
+
+
+  // On recup nos objets
+//   const basket = document.getElementById("basket");
+//   console.log(basket);
+//   const compteur = document.getElementById("compteur");
+//   const submit = document.getElementById("submit");
+
+//   // Si on clique sur le panier basket on toggle entre le basket et le compteur
+//   basket.addEventListener("click", function() {
+//     compteur.classList.toggle("show");
+//     basket.classList.toggle("hidden");
+//   });
+
+// // Si on clique sur la validation du panier on toggle dans l'autre sens
+//   submit.addEventListener("click", function() {
+//     compteur.classList.toggle("hidden");
+//     basket.classList.toggle("hidden");
+//   });
+
+
+}
+
+// export { toggleBasket };
