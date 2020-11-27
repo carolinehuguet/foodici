b01dddb585c17f7fb3c90f829604293335bee802
Nina Dissard <dissardnina@gmail.com>
2020-11-27 12:05:33 +0100

    add markers to the map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,6 +6,12 @@ const initMapbox = () => {
   // MAP -----------------------------------------------------------------------
   const mapElement = document.getElementById('map');
 
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
+
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
@@ -16,13 +22,15 @@ const initMapbox = () => {
 
     // MARKERS -----------------------------------------------------------------
 
-    // const markers = JSON.parse(mapElement.dataset.markers);
-    // markers.forEach((marker) => {
-    //   new mapboxgl.Marker()
-    //     .setLngLat([ marker.lng, marker.lat ])
-    //     .addTo(map);
-    // });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+    fitMapToMarkers(map, markers);
   }
+
 };
 
 // EXPORTS ---------------------------------------------------------------------
