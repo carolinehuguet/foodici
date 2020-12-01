f84a52578f7ca08b96f670e52ed0873fb6eec6cb
Nina Dissard <dissardnina@gmail.com>
2020-12-01 11:48:45 +0100

    Add Ajax Request to the homepage form

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,4 +3,45 @@ class ShopsController < ApplicationController
   def show
     @shops = Shop.find(params[:id])
   end
+
+  def index
+
+    if params[:transportation] == "small_walk"
+      @radius = 0.5
+    elsif params[:transportation] == "bicycle"
+      @radius = 3
+    elsif params[:transportation] == "car"
+      @radius = 10
+    else
+      @radius = 1
+    end
+
+    if params[:address].present?
+      starting_data = Geocoder.search(params[:address]).first.data
+      @starting_marker = {
+          lat: starting_data["lat"],
+          lng: starting_data["lon"]
+        }
+      @shops = Shop.near(params[:address], @radius)
+    else
+      @shops = Shop.all
+    end
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @shops.geocoded.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude,
+        infoWindow: render_to_string(partial: "pages/info_window", locals: { shop: shop }),
+        image_url: helpers.asset_url('picto/marker.svg')
+      }
+    end
+
+    @shops = Shop.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { markers: @markers, starting_marker: @starting_marker } }
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shops, only: [:show]
+  resources :shops, only: [:show, :index]
 
   resources :products, only: [:index, :show] do
     resources :order_lines, only: [:create]
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     member do
       patch :increase_quantity
       patch :decrease_quantity
- 
+
     end
   end
 
