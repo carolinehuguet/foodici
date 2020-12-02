b62814810e223138195521be7831a875d4897c12
Steven Le Moal <steven.lemoal@gmail.com>
2020-12-02 11:07:26 +0100

    fixed the link to Stripe payment. added a condition to create a cart if it doesn't exist yet when we return to the the carts show.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,11 @@
 class CartsController < ApplicationController
 
 	def show
-		@cart = current_user.orders.find_by(status: "cart")
-		# itération sur la commande afin de les regrouper par catégories
+    @cart = Order.find_or_create_by(
+      user: current_user,
+      status: "cart"
+    )
+    # itération sur la commande afin de les regrouper par catégories
 		@order_lines = @cart.order_lines.order(:created_at).group_by { |order_line| order_line.shop.category }
 		# nombre d'article dans la commande
 		@article = @cart.order_lines.count
