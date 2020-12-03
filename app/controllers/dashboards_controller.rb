660a9d4fa6f4d8fcdc92f633b528234ba9f028c0
Steven Le Moal <steven.lemoal@gmail.com>
2020-12-03 18:27:54 +0100

    added a condition to display markers in the dashboard. pending orange. fulfilled green

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,15 +6,15 @@ class DashboardsController < ApplicationController
 		@cart = @user.orders.find_by(status: status)
 		@this_order_price = 0
 
-        @shops = @cart.shops
+    @order_shops = @cart.order_shops
 
     # map
-		@markers = @shops.map do |shop|
+		@markers = @order_shops.map do |order_shop|
 			{
-				lat: shop.latitude,
-        lng: shop.longitude,
-        image_url: helpers.asset_url('picto/marker.svg'),
-        infoWindow: render_to_string(partial: "shared/dashboard/dashboard_info_window", locals: { shop: shop })
+				lat: order_shop.shop.latitude,
+        lng: order_shop.shop.longitude,
+        image_url: helpers.asset_url( order_shop.status == "fulfilled" ? 'picto/marker-approved.svg' : 'picto/marker-pending.svg'),
+        infoWindow: render_to_string(partial: "shared/dashboard/dashboard_info_window", locals: { shop: order_shop.shop })
 			}
     end
   end
